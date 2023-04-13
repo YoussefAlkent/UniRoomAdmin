6ed601d6453c22a28f8e2cb3bc05b4cae9617ad3
SpectreWasTaken <youssef.bedair@aiu.edu.eg>
2023-04-13 23:37:00 +0200

    Added Partial Blacklist Functionality

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const ejs = require('ejs')
+const sql = require('mysql')
+const nodemailer = require('nodemailer')
 
+var con = sql.createConnection({
+  host: "uni-room.mysql.database.azure.com",
+  port: 3306,
+  user:"Uniroom_Admin",
+  password:"vtaiu@12345",
+  multipleStatements:true
+})
+var transporter = nodemailer.createTransport({
+  host:'smtp.gmail.com',
+  auth:{
+      user: "", //Use an email here, Until we can create one for the entire team
+      pass: "" //Use an app specific password here if using 2fa
+  }
+})
 var app = express();
 app.listen(3000)
 app.set('views', __dirname + '\\views')
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 
 
 
 app.get('/', (req, res)=>{
   res.render('pages\\bookingInfo')
+})
+app.post('/blacklistStudent', (req, res)=>{
+  console.log("Blacklisted student")
 })
 
