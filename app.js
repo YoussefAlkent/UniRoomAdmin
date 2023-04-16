dadaffbfbfbe15ba8a3cda541469892d34fa4892
SpectreWasTaken <youssef.bedair@aiu.edu.eg>
2023-04-16 17:35:13 +0200

    WIP: building app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const ejs = require('ejs')
 const sql = require('mysql')
 const nodemailer = require('nodemailer')
+const jwt = require('jsonwebtoken')
+const secretPhrase ='OhMyGodWereDoingThisAgainPleaseChangeMeTooICannotStressThisEnoughIMPORTANT' //again, what the variable said
 
 var con = sql.createConnection({
   host: "uni-room.mysql.database.azure.com",
@@ -17,6 +19,14 @@ var transporter = nodemailer.createTransport({
       pass: "" //Use an app specific password here if using 2fa
   }
 })
+function SignInEmail(name, ID, email){
+  return {
+          from:'',
+          to:email,
+          subject:"Your Account has been signed in!",
+          text:"You have just signed in with your account"
+  }
+}
 var app = express();
 app.listen(3000)
 app.set('views', __dirname + '\\views')
@@ -24,12 +34,34 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-
-
 app.get('/', (req, res)=>{
-  res.render('pages\\bookingInfo')
+  res.render('pages\\index')
 })
-app.post('/blacklistStudent', (req, res)=>{
-  console.log("Blacklisted student")
+app.post('/login', (req, res, next)=>{
+  var [name, pass] = req.body;
+  let query = "SELECT * FROM person WHERE email=?"
+  con.query(query, name, (err, result)=>{
+    if(result.password == pass){
+      const token = jwt.sign(user, secretPhrase, {expiresIn:"3h"})
+      res.cookie('token', token, {
+        httpOnly:true
+      })
+      transporter.sendMail(SignInEmail(result.name, result.ID, result.email), (err,info)=>{
+        if (err) throw err; else console.log("email send" + info.response)
+      })
+      next();
+    } else{
+      res.body.password = ""
+    }
+  })
+}, (req, res)=>{
+  res.redirect("/bookingInfo")
+})
+app.get('/bookingInfo', (req,res)=>{
+  let query='SELECT * FROM student'
+  con.query(query, "", (err, result)=>{
+    res.render("pages\\booking", {studentList:result})
+  })
+
 })
 
